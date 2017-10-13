938d0cc76bdaade0fa0f1e296d2d7ea998550490
Massimo Santini <massimo.santini@gmail.com>
2017-10-13 03:29:11 +0200

    Fixed version finding

--- docker/tmserver/build.sh
+++ docker/tmserver/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-export VERSION=$(grep SCYTHE_SERVER_VERSION= ../scythe-server  | cut -d "=" -f2)
+export VERSION=$(grep SCYTHE_SERVER_VERSION= ../../scythe-server  | cut -d "=" -f2)
 export REPO=scythe/tmserver
 export TAG='latest'
 export COMMIT=$(git rev-parse HEAD | cut -b -8)
