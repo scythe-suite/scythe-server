3773b49a723396c681ca569e7488d1e12b6ca0f8
Massimo Santini <massimo.santini@gmail.com>
2017-11-01 10:42:48 +0100

    Transitioning from gunicorn to eventlet (for websocket support)

--- docker/stserver/entrypoint.sh
+++ docker/stserver/entrypoint.sh
@@ -4,8 +4,4 @@ if [ -z "$SCYTHE_REDIS_HOST" ]; then
     SCYTHE_REDIS_HOST=localhost
 fi
 
-if [ "$1" == "gunicorn" ]; then
-    /bin/wait-for "$SCYTHE_REDIS_HOST":6379 -- "$@"
-else
-    /bin/wait-for "$SCYTHE_REDIS_HOST":6379 -- /bin/st "$@"
-fi
+/bin/wait-for "$SCYTHE_REDIS_HOST":6379 -- /bin/st "$@"
